db9ee47992f334b2a6a3f0390839d95fd5d74432
chenyan <2545205707@qq.com>
2019-03-19 15:24:15 +0800

    add 命令脚本

---
+++ src/DpadminInit.php
@@ -0,0 +1,29 @@
+<?php
+namespace dpadmin;
+
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+use think\helper\Time;
+
+class DpadminInit extends Command
+{
+	protected function configure()
+	{
+		$this->setName('dpadmin:make')
+			->addArgument("d")
+			->setDescription("init some dpadmin cli");
+	}
+
+	/**
+	 * 执行命令
+	 * @param Input $input
+	 * @param Output $output
+	 * @return int|void|null
+	 * @throws
+	 */
+	protected function execute(Input $input, Output $output)
+	{
+
+	}
+}
